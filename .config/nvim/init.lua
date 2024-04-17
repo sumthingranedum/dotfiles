4c5cf3385460675d24595faec5fdfc0665dd2db4
Zack Waterman <zack.waterman@watermancs.com>
2024-04-16 20:46:46 -0700

    a fresh start

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,27 @@
-require("zack")
+-- Mapleader
+vim.g.mapleader = ' '
+
+-- Line Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Breakindent
+vim.opt.breakindent = true
+
+-- Smartcase Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Cursorline
+vim.opt.cursorline = false
+
+-- Scroll Buffer
+vim.opt.scrolloff = 8
+
+-- Search Highlighting
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Keybinds
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
--- .config/nvim/lua/zack/init.lua
+++
@@ -1,3 +0,0 @@
-require("zack.lazy")
-require("zack.remap")
-require("zack.set")
