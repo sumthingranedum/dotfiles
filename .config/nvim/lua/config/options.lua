c36e26b9528fafaef0b07e219cac3fb6ee0b4ab6
Zack Waterman <116687311+zackwaterman@users.noreply.github.com>
2024-07-23 23:18:30 -0700

    refactored nvim folder structure and set up lazy

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,23 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Disable mouse
+vim.opt.mouse = ""
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+vim.opt.hlsearch = true
+
+-- Transparent background
+vim.cmd('highlight Normal guibg=none ctermbg=none')
---
+++ .config/nvim/lua/config/remaps.lua
@@ -0,0 +1,10 @@
+-- Yank to clipboard
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+
+-- Delete without yanking
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
+
+-- Clear search highlight on pressing <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
