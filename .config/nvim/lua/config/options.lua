313f0a496ed9792f0a91dbe6ac8df099e0033f7f
Zack Waterman <116687311+zackwaterman@users.noreply.github.com>
2024-07-26 21:47:17 -0700

    more config stuff

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,6 +5,23 @@ vim.opt.relativenumber = true
 -- Disable mouse
 vim.opt.mouse = ""
 
+-- Tab stuff
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Disable line wrap
+vim.opt.wrap = false
+
+-- Disable swap/backup
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Disable search highlighting
+vim.opt.hlsearch = false
+
 -- Save undo history
 vim.opt.undofile = true
 
@@ -13,12 +30,13 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 8
 
-vim.opt.hlsearch = true
+-- Always leave room for lsp errors etc.
+vim.opt.signcolumn = "yes"
+
+-- Better colors
+vim.opt.termguicolors = true
 
 -- Transparent background
 vim.cmd('highlight Normal guibg=none ctermbg=none')
-
--- Use treesitter indent instead
-vim.opt.smartindent = false
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,4 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "c", "lua", "python", "rust" },
-          sync_install = false,
-	  auto_install = true,
-          highlight = { enable = true },
-        })
-    end,
 }
